ad6e583929536fac2a9afebd8ea17bbafc1b26f5
Arnaud Weyts <arnaud.weyts@gmail.com>
2018-09-15 14:59:47 +0200

    separate file for proptypes

---
+++ src/types/index.js
@@ -0,0 +1,23 @@
+import { shape, string, arrayOf, bool, func } from 'prop-types';
+
+export const orderType = shape({
+  id: string,
+  'customer-id': string,
+  items: arrayOf(
+    shape({
+      'product-id': string,
+      quantity: string,
+      'unit-price': string,
+      total: string
+    })
+  ),
+  total: string
+}).isRequired;
+
+export const ordersType = {
+  dispatch: func,
+  orders: shape({
+    isFetching: bool,
+    data: arrayOf(orderType)
+  })
+};

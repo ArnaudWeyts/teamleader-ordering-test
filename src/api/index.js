3b431ca90e358a90142b31bfc592d442ac05a105
Arnaud Weyts <arnaud.weyts@gmail.com>
2018-09-15 14:59:18 +0200

    add simualted delay to api

--- src/api/index.js
+++ src/api/index.js
@@ -4,22 +4,24 @@ import customers from './customers.json';
 import orders from './orders.json';
 import products from './products.json';
 
+const SIMULATED_DELAY = 400;
+
 export default class Api {
   getCustomers() {
     return new Promise(resolve => {
-      resolve(customers);
+      setTimeout(() => resolve(customers), SIMULATED_DELAY);
     });
   }
 
   getOrders() {
     return new Promise(resolve => {
-      resolve(orders);
+      setTimeout(() => resolve(orders), SIMULATED_DELAY);
     });
   }
 
   getProducts() {
     return new Promise(resolve => {
-      resolve(products);
+      setTimeout(() => resolve(products), SIMULATED_DELAY);
     });
   }
 }

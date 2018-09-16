1a244733c0244c58b782096745e93fcc1d91bbd6
Arnaud Weyts <arnaud.weyts@gmail.com>
2018-09-16 18:10:49 +0200

    add number input to remove multiple items

--- src/components/ProductList.jsx
+++ src/components/ProductList.jsx
@@ -1,31 +1,67 @@
-import React from 'react';
+import React, { Component } from 'react';
 
 import Product from './Product';
 
-const ProductList = props => {
-  const { items, removeProduct } = props;
+class ProductList extends Component {
+  constructor(props) {
+    super(props);
 
-  if (!items) {
-    return <div>Loading...</div>;
+    this.state = {
+      removeQuantity: 1
+    };
+
+    this.handleInputChange = this.handleInputChange.bind(this);
+    this.handleSubmit = this.handleSubmit.bind(this);
   }
 
-  return (
-    <div>
-      {items.map(item => (
-        <div key={item['product-id']}>
-          <Product product={item.product} />
-          <h3>Quantity: {item.quantity}</h3>
-          <h3>Subtotal: {item.total}</h3>
-          <button
-            type="button"
-            onClick={() => removeProduct(item['product-id'], 1)}
-          >
-            Remove product
-          </button>
-        </div>
-      ))}
-    </div>
-  );
-};
+  handleInputChange(event) {
+    const {
+      target: { name, type, checked, value }
+    } = event;
+    const newValue = type === 'checkbox' ? checked : value;
+
+    this.setState({
+      [name]: newValue
+    });
+  }
+
+  handleSubmit(e, id) {
+    e.preventDefault();
+    const { removeProduct } = this.props;
+    const { removeQuantity } = this.state;
+    removeProduct(id, removeQuantity);
+  }
+
+  render() {
+    const { items } = this.props;
+    const { removeQuantity } = this.state;
+
+    if (!items) {
+      return <div>Loading...</div>;
+    }
+
+    return (
+      <div>
+        {items.map(item => (
+          <div key={item['product-id']}>
+            <Product product={item.product} />
+            <h3>Quantity: {item.quantity}</h3>
+            <h3>Subtotal: {item.total}</h3>
+            <form onSubmit={e => this.handleSubmit(e, item['product-id'])}>
+              <input
+                type="number"
+                name="removeQuantity"
+                max={item.quantity}
+                value={removeQuantity}
+                onChange={this.handleInputChange}
+              />
+              <button type="submit">Remove product(s)</button>
+            </form>
+          </div>
+        ))}
+      </div>
+    );
+  }
+}
 
 export default ProductList;
